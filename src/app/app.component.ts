555d4f84fc46b55120ba5932e3101794258f6d2b
augustekau <83609317+augustekau@users.noreply.github.com>
2021-11-09 21:05:17 +0200

    table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { Task } from './interfaces/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-json-server';
+
+  public tasks: Task[] = [];
+
+  constructor(private _taskService: TaskService) {
+    // Gauname duomenis is task Service
+    this._taskService.getTasks().subscribe((data: Task[]) => {
+      this.tasks = data;
+      console.log(this.tasks);
+    });
+  }
 }
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../interfaces/task';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  // API Adresas is kur gausime duomenis
+  private apiUrl: string = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+}
