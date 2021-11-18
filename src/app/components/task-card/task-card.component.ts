be3dfb699c07fce54136612e8d066e0b61195961
augustekau <83609317+augustekau@users.noreply.github.com>
2021-11-18 20:20:04 +0200

    task card component

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Task } from 'src/app/interfaces/task';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.scss']
+  styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent implements OnInit {
+  // Pasirinktos uzduoties Task objektas
+  @Input() task: Task | null = null;
+  @Output() taskDetailsCloseEvent: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.task);
   }
-
+  closeTaskDetails() {
+    this.taskDetailsCloseEvent.emit(true);
+  }
 }
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Host } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../interfaces/task';
 
@@ -8,7 +8,45 @@ import { Task } from '../../interfaces/task';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  /* Klaviaturos mygtuku event listener pavyzdys */
+  @HostListener('document:keydown', ['$event'])
+  onKeyDownHandler(event: any) {
+    /* event.key - grazina paspausto mygtuko koda */
+    // console.log(event);
+    console.log(event.keyCode);
+    // Escape - keyCode yra 27
+    if (event.keyCode === 27) {
+      this.toggleTaskDetails(null, true);
+    }
+  }
   public tasks: Task[] = [];
+  // Kitamasis kuris pasako ar atvaizduoti task details komponenta
+  public showTaskDetails: boolean = false;
+
+  // Pasirinkta uzduotis, kurios informacija tures buti atvaizduojama <app-task-details> komponente
+  public selectedTask: Task | null = null;
+
+  toggleTaskDetails(task: Task | null, close: boolean = false) {
+    if (
+      this.selectedTask == task ||
+      this.showTaskDetails == false ||
+      this.selectedTask == null
+    ) {
+      this.showTaskDetails = !this.showTaskDetails;
+    }
+
+    /* Force close */
+    if (close) {
+      this.showTaskDetails = false;
+    }
+
+    this.selectedTask = task;
+
+    /* Jei uzdaromas taskDetails komponentas, selectedTask nustatoma null reiksme */
+    if (this.showTaskDetails == false) {
+      this.selectedTask = null;
+    }
+  }
 
   constructor(private _taskService: TaskService) {
     this.getTasks();
